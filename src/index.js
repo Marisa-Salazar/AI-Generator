36080b049cca854e34facbba4eefb559af2874a4
Marisa-Salazar <salazar.marisa15@gmail.com>
2023-12-30 21:28:55 -0700

    added basic form handling

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
+function generateAffirmation(event) {
+  event.preventDefault();
 
+  new Typewriter("#affirmation", {
+    strings: "La tombe dit à la rose",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+}
+
+let affirmationFormElement = document.querySelector(
+  "#affirmation-generator-form"
+);
+affirmationFormElement.addEventListener("submit", generateAffirmation);
